e014efdb99af7e88ece62c46cb584e46011d51cb
Parah <122792967+ParahDime@users.noreply.github.com>
2026-09-22 17:17:02 +0100

    Edit: tested code in place, to add column headers to code

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Land registry tracker
+
+## what it does
+
+### Languages and frameworks
+The program runs on python and its packages, as listed in main.py
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pandas as pd
 from data_utils import load_data, sanitise, report_missing, assess_nulls
-#sanitise the data
 
-#clean the data
+def testFoo():
+    print("Success")
 
+#create a report
+def analytics():
+    print("analytics")
+
+#load into sql
+
+#used to handle and initialise the data
+def openingSequence():
+    print("yeet")
 
 def main():
     print("hello world")
     #get file name
-    fileName = "lan-registry-july.csv"
+    fileName = "land-registry-july.csv"
 
     #read in the file name
-    df_raw = load_data(fileName)  #file to be read
+    df_raw = load_data("raw/" + fileName)  #file to be read
     df_raw.head()
 
     #clean the data
@@ -19,8 +28,19 @@ def main():
 
     #find any missing values
     report_missing(df)  # check what's still null before you decide what to do with it
-
-    assess_nulls(df)
     
-    df.info()
-    df.describe(include="all")
+    print(assess_nulls(df))
+    testFoo()
+    #df.info()
+    #df.describe(include="all")
+
+    #load into sql (place into function)
+
+    #the fun part (data analytics)
+
+
+main()
+#names of column headers
+"""transaction_id, price, date_of_transfer, postcode, property_type,
+old_new, duration, paon, saon, street, locality, town_city,
+district, county, ppd_category_type, record_status"""
